0c1083646f76b66eb85c16989ec6ad73b43c1046
m0dularm1nd <e.mohamedgamal@gmail.com>
2020-09-20 17:24:04 +0200

    404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,8 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import "./404.css"
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
