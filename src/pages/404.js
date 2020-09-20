a7bc54b3ad125edbbaaabb1026a90b4d4bd3bb35
m0dularm1nd <e.mohamedgamal@gmail.com>
2020-09-20 17:32:52 +0200

    last

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,8 +9,10 @@ const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
     <div className="page">
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div className="pageGroup">
+        <h1>404: Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
     </div>
   </Layout>
 )
