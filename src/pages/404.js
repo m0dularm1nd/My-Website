29acacd0f8d9314e4d4ad125203435bcd102d39f
m0dularm1nd <e.mohamedgamal@gmail.com>
2020-09-20 17:39:16 +0200

    final2

--- src/pages/404.js
+++ src/pages/404.js
@@ -14,8 +14,8 @@ const NotFoundPage = () => (
         <h1>404: Not Found</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
       </div>
+      <Wave />
     </div>
-    <Wave />
   </Layout>
 )
 
